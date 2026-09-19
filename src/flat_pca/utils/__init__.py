29a8cefcb97977604a947d0823991ba405f6c3f5
ibuka <208925568+ribuka@users.noreply.github.com>
2026-09-20 08:09:14 +0900

    feat: add natural_keys utility and pl_snippets for DataFrame operations

---
+++ src/flat_pca/utils/__init__.py
@@ -0,0 +1,11 @@
+from .natural_keys import (
+    natural_keys,
+)
+from .pl_snippets import (
+    get_shape_from_polars,
+    get_columns_from_polars,
+    get_schema_from_polars,
+    get_value_counts_from_polars,
+    get_unique_values_from_polars,
+    drop_all_null_columns_from_polars,
+)
